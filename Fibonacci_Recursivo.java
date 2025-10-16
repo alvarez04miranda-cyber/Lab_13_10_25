1d92b62acf795dc71b25c7216f1f4b5a6b303ef1
arely04alvarez <alvarez04miranda@gmail.com>
2025-10-16 00:02:25 -0400

    Refactor Fibonacci method for clarity

--- Fibonacci_Recursivo.java
+++ Fibonacci_Recursivo.java
@@ -1,28 +1,19 @@
 import java.util.Scanner;
 
-/**
- * Programa: FibonacciRecursivo.java
- * Autor: [Tu Nombre]
- * Descripción: Calcula la serie Fibonacci de manera recursiva.
- */
-
 public class Fibonacci_Recursivo {
-
-    // Método recursivo para obtener el n-ésimo número de Fibonacci
     public static int fibonacci(int n) {
-        if (n <= 1) { // Caso base
+        if (n <= 1) { 
             return n;
-        } else { // Caso recursivo
+        } else { 
             return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Ingrese la cantidad de términos de la serie Fibonacci: ");
+        System.out.print("Ingrese la cantidad de tÃ©rminos de la serie Fibonacci: ");
         int n = sc.nextInt();
 
-        System.out.println("\nSerie Fibonacci de " + n + " términos:");
+        System.out.println("\nSerie Fibonacci de " + n + " tÃ©rminos:");
         for (int i = 0; i < n; i++) {
             System.out.print(fibonacci(i) + " ");
         }
